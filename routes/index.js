162b00330a651a565854583e7d01042e983942ab
xiunaonao <onlymyalways@163.com>
2018-05-03 17:23:03 +0800

    1

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,12 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: '泓唛游戏中心' });
+});
+
+
+router.get('/chess',function(req,res,next){
+	res.render('chess',{title:'中国象棋'})
 });
 
 module.exports = router;
