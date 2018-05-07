24435281783531d8faae0822c191f8b8e2e12b4b
xiunaonao <onlymyalways@163.com>
2018-05-07 18:32:46 +0800

    init

---
+++ public/js/main.js
@@ -0,0 +1,28 @@
+
+// var chess={template:'<div></div>',
+// 	data:function(){
+// 		return {
+// 			name:"中国象棋"
+// 		}
+// 	}	
+// 	,
+// 	mounted:function(){
+		
+// 	}
+// };
+
+// var routes=[
+// 	{ path: '/chess', component: chess },
+// ]
+
+// for(var i=0;i<component.length;i++){
+// 	routes.push(component[i]);
+// }
+
+// var router=new VueRouter({
+// 	routes:routes
+// });
+
+// var app=new Vue({
+// 	router:router
+// }).$mount('#app');
